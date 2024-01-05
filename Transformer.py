c47e49d9a203befb13dab4776883d472afc80134
toto-a <antoine.h2@hotmail.com>
2024-01-05 09:40:00 +0100

    Fixed cross-attention

--- Transformer.py
+++ Transformer.py
@@ -78,9 +78,9 @@ class MultiHeadAttention(nn.Module):
         self.d_model=d_model
         self.dh=head_size
 
-        self.q=nn.Linear(d_model,head_size)
-        self.k=nn.Linear(d_model,head_size)
-        self.v=nn.Linear(d_model,head_size)         
+        self.q=nn.Linear(d_model,d_model)
+        self.k=nn.Linear(d_model,d_model)
+        self.v=nn.Linear(d_model,d_model)         
         self.dropout=nn.Dropout(dropout)
 
        
@@ -116,12 +116,12 @@ class MultiHeadAttention(nn.Module):
         B,seq_len,embd=q.shape
 
         # (Batch,seq_len,d_model)->(Batch,seq_len,n_head,head_size)->(Batch,n_head,seq_len,head_size)
-        q=q.view(-1,self.h,self.dh).transpose(1,2)
-        k=k.view(-1,self.h,self.dh).transpose(1,2)
-        v=v.view(-1,self.h,self.dh).transpose(1,2)
+        q=q.view(B,seq_len,self.h,self.dh).transpose(1,2)
+        k=k.view(B,seq_len,self.h,self.dh).transpose(1,2)
+        v=v.view(B,seq_len,self.h,self.dh).transpose(1,2)
 
         x,attention_score=MultiHeadAttention.attention(q,k,v,mask,self.dropout)
-        x=x.transpose(1,2).contiguous().view(-1,self.h*self.dh)#(Batch,n_head,seq_len,head_size)->(Batch,seq_len,n_head,head_size) ->(Batch,seq_len,d_model)
+        x=x.transpose(1,2).contiguous().view(B,seq_len,self.h*self.dh)#(Batch,n_head,seq_len,head_size)->(Batch,seq_len,n_head,head_size) ->(Batch,seq_len,d_model)
 
         out=self.proj(x)
 
@@ -130,20 +130,21 @@ class MultiHeadAttention(nn.Module):
 
 
 class Encoder(nn.Module):
-    def __init__(self, d_model, n_head) -> None:
+    def __init__(self, d_model, n_head,d_ff) -> None:
         super().__init__()
         
         assert d_model % n_head ==0
         
         self.sa_heads=MultiHeadAttention(d_model=d_model,num_head=n_head,head_size=d_model//n_head)
-        self.mlp=FeedForward(d_model)
+        self.mlp=FeedForward(d_model,d_ff)
         self.ln1=nn.LayerNorm(d_model)
         self.ln2=nn.LayerNorm(d_model)
     
-    def forward(self,x):
-        out=self.sa_heads(self.ln1(x))
+    def forward(self,x,mask):
+        _x=self.ln1(x)
+        out=self.sa_heads(_x,_x,_x,mask)
         out=out + x
-        out=self.ln2(self.mlp(out)) + out
+        out=self.mlp(out) + out
 
         return out
 
@@ -151,12 +152,13 @@ class Encoder(nn.Module):
 class EncoderBlock(nn.Module) :
     def __init__(self,n_layers,d_model,n_head) -> None:
         super().__init__()
-        self.encoder_block=nn.Sequential(*[Encoder(d_model=d_model,n_head=n_head) for _ in range(n_layers)])
+        self.encoder_block=nn.ModuleList([Encoder(d_model=d_model,n_head=n_head,d_ff=4*d_model) for _ in range(n_layers)])
         self.ln=nn.LayerNorm(d_model)
     
 
-    def forward(self,x):
-        out=self.encoder_block(x)
+    def forward(self,x,src_mask):
+        for layers in self.encoder_block :
+            out=layers(x,src_mask)
         out=self.ln(out)
 
         return out
@@ -164,30 +166,42 @@ class EncoderBlock(nn.Module) :
 
 
 class Decoder(nn.Module):
-    def __init__(self ,n_layers,d_model,n_head):
+    def __init__(self ,d_model,n_head):
         super().__init__()
         self.sa_heads=MultiHeadAttention(d_model=d_model,num_head=n_head,head_size=d_model//n_head)
         self.cross_att=MultiHeadAttention(d_model=d_model,num_head=n_head,head_size=d_model//n_head)
-        self.mlp=FeedForward(d_model)
-        self.ln1=nn.LayerNorm(d_model)
-        self.ln2=nn.LayerNorm(d_model)
 
-    def forward(self,x):
-        out=self.sa_heads(self.ln1(x),mask=torch.tril(torch.ones((x.shape[1],x.shape[1]))))
+        self.mlp=FeedForward(d_model,d_ff=4*d_model)
+        self.ln1=nn.LayerNorm(d_model)
+        self.ln3=nn.LayerNorm(d_model)
+        self.dropout=nn.Dropout(dropout)
+
+    def forward(self,encoder_output,x,mask_src,mask_tgt):
+        _x=self.ln1(x)
+        out=self.sa_heads(_x,_x,_x,mask_tgt) #causal mask -attention
         out=out + x
-        out=self.cross_att(out)
-        out=self.ln2(self.mlp(out)) + out
+
+        if encoder_output is  not None : 
+            out=self.cross_att(out,encoder_output,encoder_output,mask_src)
+            out=self.dropout(out)
+        
+        out=self.mlp(out) + out
 
         return out
     
 class DecoderBlock(nn.Module) :
     def __init__(self,n_layers,d_model,n_head) -> None:
         super().__init__()
-        self.decoder_block=nn.Sequential(*[Decoder(d_model=d_model,n_head=n_head) for _ in range(n_layers)])
+        self.decoder_block=nn.ModuleList([Decoder(d_model=d_model,n_head=n_head) for _ in range(n_layers)])
+        self.ln=nn.LayerNorm(d_model)
+
     
 
-    def forward(self,x):
-        out=self.decoder_block(x)
+    def forward(self,encoder_output,x,src_mask,tgt_mask):
+        _x=x
+        for layer in self.decoder_block :
+            out=layer(_x,encoder_output,src_mask,tgt_mask)
+
         out=self.ln(out)
 
         return out
@@ -199,7 +213,7 @@ class ProjectionLayer(nn.Module):
         self.proj=nn.Linear(d_model,vocab_size)
     
     def forward(self,x):
-        return torch.log_softmax(self.proj(x))
+        return torch.log_softmax(self.proj(x),dim=-1,dtype=torch.int64)
 
 
 
@@ -243,8 +257,8 @@ def build_transformers(src_vocab_size,tgt_vocab_size,src_seq_len,tgt_seq_len,d_m
 
 
     ##Encoder/ Deocder Block
-    encoder_blocks=EncoderBlock(N,d_model=d_model,n_head=h)
-    decoder_blocks=DecoderBlock(N,d_model=d_model,n_head=h)
+    encoder_blocks=EncoderBlock(n_layers=N,d_model=d_model,n_head=h)
+    decoder_blocks=DecoderBlock(n_layers=N,d_model=d_model,n_head=h)
 
 
     projection_layer=ProjectionLayer(d_model,tgt_vocab_size)
