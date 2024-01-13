1fb3c66b003d05901f62a67995801727cff0e015
toto-a <antoine.h2@hotmail.com>
2024-01-13 20:05:05 +0100

    gitignore

--- Build_Transformers/train.py
+++ Build_Transformers/train.py
@@ -205,7 +205,7 @@ def train_model(config):
             #Run through the transformer
             encoder_output=model.encode(encoder_input,encoder_mask)
             decoder_output=model.decode(encoder_output,encoder_mask,decoder_input,decoder_mask) #(Batchn seq_len, d_model)
-            proj_output=model.proj(decoder_output) #(Batch, seq_len , vocan_size)
+            proj_output=model.proj(decoder_output) #(Batch, seq_len , vocab_size)
 
             label=batch["label"].to(device) #(Batch,seq_len)
 
