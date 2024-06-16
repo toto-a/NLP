de9b6f8e07d82d622f8b1eee1437f384a897d65e
toto <antoine.h2@hotmail.com>
2024-06-16 18:56:40 +0200

    tranformer.py

--- Modern_Transformers/transformers.py
+++ Modern_Transformers/transformers.py
@@ -1,8 +1,30 @@
 import torch
 import torch.nn as nn     
 import torch.nn.functional as F   
+from dataclasses import dataclass
 
-from transformer_components import CausalAttention,MLP,RMSNorm
+from transformer_components import CausalAttention,MLP,RMSNorm,CausalAttentionKVCache,PositionEmbeddings
+
+@dataclass
+class TransformerConfig :
+    seq_len : int =512
+    n_heads : int =12
+    hidden_size : int = 768 
+    k_hidden :int =4
+    eps : float = 1e-12
+
+
+    ##Flag
+    return_pe=True
+
+
+
+config=TransformerConfig()
+
+
+pos={'sinusoid ' : PositionEmbeddings(config)}
+att={"self_attention": CausalAttention(config), "kv_attention":CausalAttentionKVCache(config)}
+norm={"rms":RMSNorm(config),"ln":nn.LayerNorm(config.hidden_size, config.eps)}
 
 
 
@@ -26,3 +48,6 @@ class Block(nn.Module) :
         r = self.feed_forward(self.ffn_norm(h))
         out = h + r
         return out
+
+
+
